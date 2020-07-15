1463f94f5ba8b7e4cd5b8193841f114bdab7b4e1
kudlifeMacBook <ljunyfor@outlook.com>
2020-07-15 21:10:25 +0800

    server complete

--- src/oxyethylene/client/PingClient.java
+++ src/oxyethylene/client/PingClient.java
@@ -1,4 +1,95 @@
 package oxyethylene.client;
 
+import java.io.IOException;
+import java.net.*;
+import java.util.Date;
+import java.util.concurrent.*;
+
 public class PingClient {
+    public static void main(String[] args) {
+        // 参数处理
+        if (args.length < 2) {
+            System.out.println("输入的参数过少。使用方法：PingClient host port");
+        }
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        // 创建socket
+        try (DatagramSocket client = new DatagramSocket(1234, InetAddress.getByName(host))) {
+            System.out.println("ping " + host + ":" + port);
+            long minTime = 1001;
+            long maxTime = 0;
+            long avgTime = 0;
+            int lossCount = 0;
+            for (int i = 0; i < 10; i++) {
+                // 发送请求
+                long startTime = new Date().getTime();
+                byte[] payload = buildPayload(i, startTime);
+                DatagramPacket requestPacket
+                        = new DatagramPacket(payload, payload.length, InetAddress.getByName(host), port);
+                client.send(requestPacket);
+
+                Callable<DatagramPacket> waitForReplyTask = new Callable<DatagramPacket>() {
+                    @Override
+                    public DatagramPacket call() throws Exception {
+                        byte[] buf = new byte[1024];
+                        DatagramPacket replyPacket = new DatagramPacket(buf, buf.length);
+                        client.receive(replyPacket);
+                        return replyPacket;
+                    }
+                };
+                ExecutorService executorService = Executors.newFixedThreadPool(1);
+                Future<DatagramPacket> replyPacketFuture = executorService.submit(waitForReplyTask);
+                try {
+                    DatagramPacket replyPacket = replyPacketFuture.get(3, TimeUnit.SECONDS);
+                    long endTime = new Date().getTime();
+                    long timeCost = endTime - startTime;
+                    if (timeCost > maxTime) {
+                        maxTime = timeCost;
+                    }
+                    if (timeCost < minTime) {
+                        minTime = timeCost;
+                    }
+                    avgTime += timeCost;
+                    System.out.println(replyPacket.getLength() + " bytes, " + timeCost + " ms, seq=" + i);
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                } catch (TimeoutException te) {
+                    System.out.println("等待超时");
+                    ++lossCount;
+                }finally {
+                    executorService.shutdownNow();
+                }
+            } // for end here
+            System.out.println("10 packets transmitted, "
+                    + (10 - lossCount) + " packets received, "
+                    + (double) lossCount / 10.0 * 100 + "% packet loss");
+            System.out.println("round-trip min/avg/max/ = "
+                    + minTime + "/"
+                    + (lossCount == 10 ? "" : (avgTime / (long) (10 - lossCount) + "/"))
+                    + maxTime + " ms");
+        } catch (UnknownHostException | SocketException e) {
+            System.out.println("无法创建Socket");
+            e.printStackTrace();
+            System.exit(-1);
+        } catch (IOException ioe) {
+            System.out.println("无法发送数据包");
+            ioe.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+
+    /**
+     * 将信息拼装成请求消息数组
+     *
+     * @param SequenceNumber 序号
+     * @param TimeStamp      时间戳，发送该消息的机器时间
+     */
+    private static byte[] buildPayload(int SequenceNumber, long TimeStamp) {
+        String payload = "PingUDP "
+                + "HEAD:" + Thread.currentThread().getName()
+                + "SequenceNumber:" + SequenceNumber
+                + "TimeStamp:" + TimeStamp;
+        return payload.getBytes();
+    }
 }
--- src/oxyethylene/server/PingServer.java
+++ src/oxyethylene/server/PingServer.java
@@ -1,4 +1,84 @@
 package oxyethylene.server;
 
+import java.io.IOException;
+import java.net.*;
+
 public class PingServer {
+    private static DatagramSocket server;
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("输入的参数过少。使用方法：PingServer port");
+            System.exit(-1);
+        }
+        int port = Integer.parseInt(args[0]);
+
+        try {
+            // 建立连接
+            server = new DatagramSocket(port);
+            System.out.println("已在127.0.0.1: " + port + "创建DatagrameSocket");
+            // 持续监听端口，获得数据
+            while (true) {
+                byte[] buf = new byte[1024];
+                DatagramPacket receivedPacket = new DatagramPacket(buf, buf.length);
+                server.receive(receivedPacket);
+                new Thread(new RequestHandler(receivedPacket)).start();
+            }
+        } catch (SocketException se) {
+            System.out.println("创建Socket时发生错误");
+            se.printStackTrace();
+            System.exit(-1);
+        } catch (IOException ioe) {
+            System.out.println("接受数据时出错");
+            ioe.printStackTrace();
+            System.exit(-1);
+        }
+        server.close();
+    }
+
+    static class RequestHandler implements Runnable {
+        // 模拟的延迟的最大值，有500用于模拟丢失
+        private static final int MAX_DELAY = 1500;
+        private final DatagramPacket receivedPacket;
+
+        public RequestHandler(DatagramPacket receivedPacket) {
+            this.receivedPacket = receivedPacket;
+        }
+
+        @Override
+        public void run() {
+            // 接收报文的处理
+            String payload = new String(receivedPacket.getData()).trim();
+            int port = receivedPacket.getPort();
+            InetAddress address = receivedPacket.getAddress();
+            System.out.println("收到来自" + address.toString() + ":" + port + "的消息");
+            System.out.println("Payload: " + payload + "\n");
+
+            // 模拟延迟
+            long delay = (long) (Math.random() * MAX_DELAY);
+            // 模拟数据丢失
+            if (delay > 1000) {
+                System.out.println("回复数据丢失\n");
+                return;
+            }
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+
+            // 发送回复
+            byte[] replyPayload = ("reply to " + address.toString() + ":" + port).getBytes();
+            DatagramPacket reply = new DatagramPacket(replyPayload, replyPayload.length, address, port);
+            try {
+                server.send(reply);
+                System.out.println("已发送对" + address.toString() + ":" + port + "的回复\n");
+            } catch (IOException ioe) {
+                System.out.println("发送回复时出现错误\n");
+                ioe.printStackTrace();
+            }
+
+        }
+    }
+
 }
